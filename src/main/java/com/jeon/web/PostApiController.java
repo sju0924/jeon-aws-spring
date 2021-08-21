073a589722b0a2fad130dc5ab0d2f4d2e3cd20b2
ZION <sju0924@khu.ac.kr>
2021-08-21 13:07:09 +0900

    update update and delete

--- src/main/java/com/jeon/web/PostApiController.java
+++ src/main/java/com/jeon/web/PostApiController.java
@@ -1,11 +1,14 @@
 package com.jeon.web;
 
+import com.jeon.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import com.jeon.service.posts.PostsService;
 import com.jeon.web.dto.PostsResponseDto;
 import com.jeon.web.dto.PostsSaveRequestDto;
 import com.jeon.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,13 +22,23 @@ public class PostApiController {
 
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long Id, @RequestBody PostsUpdateRequestDto requestDto){
-        return postsService.update(Id,requestDto);
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 
 
